2b2ac65ed987e36529900b84a770dea2883d7cb2
Bryan Lund <bryan.helge@gmail.com>
2017-12-02 12:26:08 -0700

    added pages home gallery about reader

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def gallery
+  end
+
+  def reader
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/gallery'
+
+  get 'pages/reader'
+
+  get 'pages/about'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
